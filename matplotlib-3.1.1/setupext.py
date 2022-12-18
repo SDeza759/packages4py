8db86c3ac3069ff95347a63feba37f551533740d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 23:07:39 +0000

    Adding tarfile member sanitization to extractall()

--- matplotlib-3.1.1/setupext.py
+++ matplotlib-3.1.1/setupext.py
@@ -744,7 +744,26 @@ class FreeType(SetupPackage):
             tar_contents.seek(0)
             with tarfile.open(tarball, mode="r:gz",
                               fileobj=tar_contents) as tgz:
-                tgz.extractall("build")
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tgz, "build")
 
         print("Building freetype in {}".format(src_path))
         if sys.platform != 'win32':
